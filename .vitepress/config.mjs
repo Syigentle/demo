0d6b3b698d4491b353794362365d1c5a921b8773
18858359855 <codersyigentle@163.com>
2024-07-10 00:55:52 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,76 @@
+import { defineConfig } from "vitepress"
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+    title: "Syigenle文档",
+    description: "A VitePress Site", // SEO用到的，无需太关注
+    // 网页图标
+    head: [
+        [
+            "link",
+            {
+                rel: "icon",
+                href: "/logo.svg",
+            },
+        ],
+    ],
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        logo: "/logo.svg", // 左上角的logo
+        nav: [
+            { text: "主页", link: "/" },
+            {
+                text: "下拉框",
+                items: [
+                    { text: "示例", link: "/markdown-examples" },
+                    { text: "Api", link: "/api-examples" },
+                ],
+            },
+        ],
+
+        sidebar: [
+            {
+                text: "Examples",
+                items: [
+                    { text: "Markdown Examples", link: "/markdown-examples" },
+                    { text: "Runtime API Examples", link: "/api-examples" },
+                ],
+            },
+            {
+                text: "Examples2",
+                items: [
+                    { text: "Markdown Examples", link: "/markdown-examples" },
+                    { text: "Runtime API Examples", link: "/api-examples" },
+                ],
+            },
+        ],
+
+        socialLinks: [
+            { icon: "github", link: "https://github.com/vuejs/vitepress" },
+            { icon: "twitter", link: "https://twitter.com/vuejs" },
+        ],
+        search: {
+            provider: "local",
+            options: {
+                translations: {
+                    button: {
+                        buttonText: "搜索文档",
+                        buttonAriaLabel: "搜索文档",
+                    },
+                    modal: {
+                        noResultsText: "无法找到相关结果",
+                        resetButtonTitle: "清除查询条件",
+                        footer: {
+                            selectText: "选择",
+                            navigateText: "切换",
+                        },
+                    },
+                },
+            },
+        },
+
+        footer: {
+            copyright: "Copyright © 2024 Syigentle",
+        },
+    },
+})
---
+++ index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+    name: "Syigentle的VitePress"
+    text: "一个VitePress示例"
+    tagline: 每天都要努力
+    image: /background.svg
+    actions:
+        - theme: brand
+          text: 跳转百度
+          link: http://www.baidu.com
+        - theme: alt
+          text: API文档
+          link: /api-examples
+
+features:
+    - title: Feature A
+      details: 特征1
+    - title: Feature B
+      details: 特征2
+    - title: Feature C
+      details: 特征3
+---
