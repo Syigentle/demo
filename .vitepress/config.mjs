ce0b2baadd9f051126ee0caadb5ac9cdc265b1cd
18858359855 <codersyigentle@163.com>
2024-07-10 01:32:47 +0800

    网页图标地址修改

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
             "link",
             {
                 rel: "icon",
-                href: "/logo.svg",
+                href: "/demo/logo.svg",
             },
         ],
     ],
